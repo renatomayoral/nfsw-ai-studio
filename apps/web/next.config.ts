17adaab9c3c9c52536ff95d8feb00e222c809638
renatomayoral <renatomayoral@gmail.com>
2026-06-17 10:17:37 -0300

    chore: remove .env.example and configure dotenv in relevant files

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,4 +1,8 @@
+import { config } from 'dotenv'
+import path from 'path'
 import type { NextConfig } from 'next'
+
+config({ path: path.resolve(__dirname, '../../.env') })
 
 const nextConfig: NextConfig = {
   output: 'standalone',
